47ef22a10a90ff0826b96284b2d0e17c5071e90a
deok-beom <baek940dog@gmail.com>
2023-05-24 11:59:39 +0900

    Solve: Step#1 no.2557

---
+++ src/main/java/step1/_1_2557.java
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/2557
+package step1;
+
+public class _1_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
