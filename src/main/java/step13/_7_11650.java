6243e40d18557f29849e0286bf0390c1263a2460
deokbeom <baek940dog@gmail.com>
2023-06-20 17:40:02 +0900

    Solve: Step#13 no.11650, 11651

---
+++ src/main/java/step13/_7_11650.java
@@ -0,0 +1,37 @@
+// https://www.acmicpc.net/problem/11650
+package step13;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _7_11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] coordinates = new int[N][2];
+
+        for (int i = 0; i < coordinates.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coordinates[i][0] = Integer.parseInt(st.nextToken());
+            coordinates[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+
+        Arrays.sort(coordinates, (c1, c2) -> {
+            if (c1[0] == c2[0]) {
+                return c1[1] - c2[1];
+            } else {
+                return c1[0] - c2[0];
+            }
+        });
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int[] coordinate : coordinates) {
+            bw.write(coordinate[0] + " " + coordinate[1] + System.lineSeparator());
+        }
+        bw.flush();
+        bw.close();
+    }
+}
