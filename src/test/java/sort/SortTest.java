5fe2dbdd1776bba25971071203e862a435653572
deokbeom <baek940dog@gmail.com>
2023-06-23 18:05:48 +0900

    Change: SortTest

--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -33,6 +33,19 @@ class SortTest {
     }
 
     @Test
+    void insertionSort() {
+        // Given
+        int[] arr = new int[]{5, 7, 10, 1, 4, 6, 0, 8, 9, 3};
+
+        // When
+        InsertionSort.sort(arr);
+
+        // Then
+        System.out.println("arr = " + Arrays.toString(arr));
+        Assertions.assertArrayEquals(new int[]{0, 1, 3, 4, 5, 6, 7, 8, 9, 10}, arr);
+    }
+
+    @Test
     void quickSort() {
         // Given
         int[] arr = new int[]{5, 7, 10, 1, 4, 6, 0, 8, 9, 3};
