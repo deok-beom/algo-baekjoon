2e21594f98d7d11d3865ce40d142abbf8afe438f
deok-beom <baek940dog@gmail.com>
2023-06-27 15:11:09 +0900

    Solve: Step#13 no.11478

---
+++ src/main/java/step14/_8_11478.java
@@ -0,0 +1,23 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _8_11478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        Set<String> substring = new HashSet<>();
+
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = i + 1; j <= S.length(); j++) {
+                substring.add(S.substring(i, j));
+            }
+        }
+
+        System.out.print(substring.size());
+    }
+}
